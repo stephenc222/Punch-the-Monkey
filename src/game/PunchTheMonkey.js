f8e641a1a2211ee02f79cdfad16e7f4389d4df07
Stephen <stephenc211@gmail.com>
2016-09-17 05:17:33 +0000

    Goodbye ball, hello chimp ( removed yellow circle, shrank monkey base sprite)

--- src/game/PunchTheMonkey.js
+++ src/game/PunchTheMonkey.js
@@ -8,8 +8,8 @@ export class PunchTheMonkey {
   _init() {
     document.title = 'Punch the Monkey';
     this.canvas = document.createElement('canvas');
-    this.canvas.width = 640;
-    this.canvas.height = 480;
+    this.canvas.width = 640; // 640 originally
+    this.canvas.height = 480; // 480 orig
     document.body.insertBefore(this.canvas, document.body.firstChild);
     this.ctx = this.canvas.getContext('2d');
   }
@@ -17,7 +17,7 @@ export class PunchTheMonkey {
   _create() {
     const { ctx, canvas } = this;
     const chimp_base = new Image();
-    chimp_base.src = './chimp_base.png';
+    chimp_base.src = './chimp_base.1.png';
 
     const chimp = {
       speed: 300,
@@ -28,7 +28,7 @@ export class PunchTheMonkey {
       // TODO color prop is temporary, just to help with sprite alignment
       color: 'yellow',
       image: chimp_base,
-      radius: 32,
+      radius: 42, // adjusted from 60 to better fit chimp base sprite
       
       reset() {
         chimp.vx = -1 + Math.random() * 1;
@@ -64,12 +64,10 @@ export class PunchTheMonkey {
       ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.save();
       ctx.translate(chimp.x - chimp.radius, chimp.y - chimp.radius);
-      ctx.fillStyle = chimp.color; // testing png alignment
       // TODO need to resuze chimp -much- smaller, better align it to the center of the chimp object
-      ctx.drawImage(chimp.image, 0, 0); //TESTING
+      ctx.drawImage(chimp.image, 0, 0);
       ctx.beginPath();
       ctx.arc(chimp.radius, chimp.radius, chimp.radius, 0, 2 * Math.PI);
-      ctx.fill();
       ctx.restore();
     };
 
