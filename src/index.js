10fe136a7186db18b25b25e0982966c5f29c6867
Stephen <stephenc211@gmail.com>
2016-10-02 03:41:32 +0000

    added visuals for lives and score counters, and added start screen

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 
 // bring in the bouncing ball game
+import {StartGame} from './game/PunchTheMonkey';
 import { PunchTheMonkey } from './game/PunchTheMonkey';
 
 function boot() {
-  // kick off your own game here when you are ready
   
-  // check out the bouncing ball game
-  const game = new PunchTheMonkey();
-}
+  const start = new StartGame();
 
+  //setTimeout(mainGame(), 5000);
+  
+  const menu = document.getElementById('startScreen');
+  menu.addEventListener('click', (event) => {
+    if(event){
+      ClearScreen('startScreen');
+      mainGame();
+    }
+  });
+  
+  
+  function ClearScreen(id){
+    // check out the bouncing ball game
+    const test = document.getElementById(id);
+    test.parentNode.removeChild(test);
+  }
+  function mainGame () {
+    const game = new PunchTheMonkey();
+  }
+  
+    //let test = document.getElementById('myCanvas');
+    //test.parentNode.removeChild(test);
+ // }
+}
 // this starts our boot function once the page is loaded
 document.addEventListener('DOMContentLoaded', boot, false);
